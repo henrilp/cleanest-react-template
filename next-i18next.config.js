43436d693f85c169a7b84ac18e1f642af03c3023
Henri <henrilepage0@gmail.com>
2022-03-23 11:32:34 +0100

    i18n management: next-i18next

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+// for dynamic configs : https://github.com/isaachinman/next-i18next#unserialisable-configs
+
+module.exports = {
+  i18n: {
+    defaultLocale: "fr",
+    locales: ["en", "fr"],
+    localePath: "./locales",
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 const Home: NextPage = () => {
+  const { t } = useTranslation();
   return (
     <div className={styles.container}>
       <Head>
@@ -17,8 +20,10 @@ const Home: NextPage = () => {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
+        <h2>LANG = {t("lang")}</h2>
+
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +64,21 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
+};
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      // allows to give only the appropriate language to browser (SSG)
+      ...(await serverSideTranslations(locale)),
+    },
+  };
 }
-
-export default Home
+export default Home;
