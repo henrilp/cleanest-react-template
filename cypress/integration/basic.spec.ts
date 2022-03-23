ae54f78c52d0b7016b9c2814f59fff75396b2d61
Henri <henrilepage0@gmail.com>
2022-03-23 12:27:34 +0100

    cypress config + PORT diff --git a/.env.development b/.env.development deleted file mode 100644 index 76e0d87..0000000 --- a/.env.development +++ /dev/null @@ -1,3 +0,0 @@ -HOSTNAME=localhost -PORT=3333 -HOST=http://$HOSTNAME:$PORT \ No newline at end of file diff --git a/cypress.json b/cypress.json index 0967ef4..a28f739 100644 --- a/cypress.json +++ b/cypress.json @@ -1 +1,3 @@ -{} +{ +    "baseUrl": "http://localhost:3333" +} \ No newline at end of file diff --git a/cypress/integration/basic.spec.ts b/cypress/integration/basic.spec.ts index af1b626..0541795 100644 --- a/cypress/integration/basic.spec.ts +++ b/cypress/integration/basic.spec.ts @@ -6,24 +6,26 @@ context("Basic", () => {    it("basic nav", () => {      cy.url().should("eq", "http://localhost:3333/");
    -    cy.contains("[Home Layout]").should("exist");
    +    // cy.contains("[Home Layout]").should("exist");
    
    -    cy.get("#input")
    -      .type("Vitesse{Enter}")
    -      .url()
    -      .should("eq", "http://localhost:3333/hi/Vitesse");
    +    // cy.get("#input")
    +    //   .type("Vitesse{Enter}")
    +    //   .url()
    +    //   .should("eq", "http://localhost:3333/hi/Vitesse");
    
    -    cy.contains("[Default Layout]").should("exist");
    +    // cy.contains("[Default Layout]").should("exist");
    
    -    cy.get(".btn").click().url().should("eq", "http://localhost:3333/");
    +    // cy.get(".btn").click().url().should("eq", "http://localhost:3333/");
       });
    
    -  it("markdown", () => {
    -    cy.get('[title="About"]')
    -      .click()
    -      .url()
    -      .should("eq", "http://localhost:3333/about");
    +  // it("markdown", () => {
    +  //   cy.get('[title="About"]')
    +  //     .click()
    +  //     .url()
    +  //     .should("eq", "http://localhost:3333/about");
    
    -    cy.get("pre.language-js").should("exist");
    -  });
    +  //   cy.get("pre.language-js").should("exist");
    +  // });
     });
    +
    +export {}; // to prevent ts "isolated modules" error
    diff --git a/package.json b/package.json
    index bb0c3d6..d601d90 100644
    --- a/package.json
    +++ b/package.json
    @@ -3,7 +3,7 @@
       "version": "0.1.0",
       "private": true,
       "scripts": {
    -    "dev": "next dev",
    +    "dev": "next dev -p 3333",
         "build": "next build",
         "start": "next start",
         "lint": "next lint",
    @@ -25,4 +25,4 @@
         "sass": "^1.49.9",
         "typescript": "4.6.2"
       }
    -}
    +}
    \ No newline at end of file

--- cypress/integration/basic.spec.ts
+++ cypress/integration/basic.spec.ts
@@ -6,24 +6,26 @@ context("Basic", () => {
   it("basic nav", () => {
     cy.url().should("eq", "http://localhost:3333/");
 
-    cy.contains("[Home Layout]").should("exist");
+    // cy.contains("[Home Layout]").should("exist");
 
-    cy.get("#input")
-      .type("Vitesse{Enter}")
-      .url()
-      .should("eq", "http://localhost:3333/hi/Vitesse");
+    // cy.get("#input")
+    //   .type("Vitesse{Enter}")
+    //   .url()
+    //   .should("eq", "http://localhost:3333/hi/Vitesse");
 
-    cy.contains("[Default Layout]").should("exist");
+    // cy.contains("[Default Layout]").should("exist");
 
-    cy.get(".btn").click().url().should("eq", "http://localhost:3333/");
+    // cy.get(".btn").click().url().should("eq", "http://localhost:3333/");
   });
 
-  it("markdown", () => {
-    cy.get('[title="About"]')
-      .click()
-      .url()
-      .should("eq", "http://localhost:3333/about");
+  // it("markdown", () => {
+  //   cy.get('[title="About"]')
+  //     .click()
+  //     .url()
+  //     .should("eq", "http://localhost:3333/about");
 
-    cy.get("pre.language-js").should("exist");
-  });
+  //   cy.get("pre.language-js").should("exist");
+  // });
 });
+
+export {}; // to prevent ts "isolated modules" error
