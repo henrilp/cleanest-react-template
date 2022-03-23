8f95ea99796bc2d6b579a4f1d3137de663a75f41
Henri <henrilepage0@gmail.com>
2022-03-23 10:44:45 +0100

    Revert "add React-i18next feature"
    This reverts commit 4488202434643fd728a8429be4c500a1527f0e22.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,8 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-// module i18n
-// can be dynamically ("lazily") imported via next "dynamic" property
-import "../modules/i18n";
-
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
