19e6d8c0fee824502d89f5e3400beb98d7796c97
Henri <henrilepage0@gmail.com>
2022-03-23 11:37:39 +0100

    better comments

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,4 +1,9 @@
-// for dynamic configs : https://github.com/isaachinman/next-i18next#unserialisable-configs
+/**
+ * for dynamic configs : https://github.com/isaachinman/next-i18next#unserialisable-configs
+ *
+ * changing language dynamically from browser shouldn't work
+ * as we load only necessary translations from getStaticProps (SSG)
+ */
 
 module.exports = {
   i18n: {
